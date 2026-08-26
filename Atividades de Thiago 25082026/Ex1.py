015a5a104eaebd2c73696dfcbc618b12aa09f32b
mariolobo-code <mario.lobo@escola.pr.gov.br>
2026-08-25 22:05:01 -0300

    Add files via upload

---
+++ Atividades de Thiago 25082026/Ex1.py
@@ -0,0 +1,11 @@
+soma = 0
+numero = int(input("digite o numero"))
+
+if numero < 10 or numero > 20:
+    print(" o numero esta fora intervalo entre 10 a 20 ")
+else :
+    print(" o numero esta dentro intervalo")
+
+
+
+    
---
+++ Atividades de Thiago 25082026/Ex2.py
@@ -0,0 +1,15 @@
+soma = 0
+
+while True:
+      n = input("Digite um numero ou sair:")
+
+      entrada = input("Digite um número (ou 'sair'): ")
+      if n == "sair":
+             break
+      
+      else:
+         soma += int(n)
+      
+
+      print(soma)
+    
